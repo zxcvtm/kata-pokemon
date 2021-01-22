c499117aa01b2a08829ca9409f081d0d2112a710
Juan Lopez <juan.ign.lpz@gmail.com>
2021-01-22 15:47:46 -0300

    styles al listado y se agrego states

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import './App.css';
+import { useState } from 'react'
+import axios from 'axios'
 
-const App = () => {
-  const list = [
+const list = [
     {name: 'pikachu', url: 'x'},
     {name: 'charmander', url: 'x'},
     {name: 'ditto', url: 'x'},
     {name: 'cubone', url: 'x'},
-  ]
+]
+
+const App = () => {
+  axios.get('https://pokeapi.co/api/v2/pokemon?limit=100&offset=200')
+  .then(response => console.log('response: ', response))
+  .catch(error => console.log('error: ', error))
+
+  const [pokemonList, setPokemonList] = useState(list)
+
+  const addPokemon = () => {
+    console.log('add pokemon is called')
+    setPokemonList([...pokemonList, {name: 'arbok', url: 'x'}])
+  }
+
   return (
     <div style={{margin: '1rem'}}>
       <h1>Kata Pokemon</h1>
-      {list.map((pokemon, index) => (
-        <div key={index}>
+      {pokemonList.map((pokemon, index) => (
+        <div key={index} style={{margin: '1rem 0', border: '1px solid black', padding: '1rem'}}>
           <span>{pokemon.name}</span>
         </div>
       ))}
+      <button onClick={addPokemon}>Agregar Pokemon</button>
     </div>
   );
 }
